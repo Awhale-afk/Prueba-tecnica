843940ea2f85d694dd138fc8ded102defb613a36
Emmanuel <est.emmgarcia.edu.co@gmail.com>
2026-03-23 15:23:18 -0500

    alert del botón de envío

--- logica.js
+++ logica.js
@@ -18,9 +18,10 @@ btnEnviar.addEventListener('click', function (c) { //event.listener relacionado 
         return
     }else{
 
+        alert("Sus comentarios han sido recibidos");
         const URL_SCRIPT = "https://script.google.com/macros/s/AKfycbxBHOaRdCr7qfotrunHBG2psZoDEyxtEq-b_PSS4wCehFI4_msLb3t3PL2ss9s1DBGX/exec" //URL de Apps Script//
 
-        let datos = new URLSearchParams();
+        let datos = new URLSearchParams(); //creación de objeto que contiene la información del formulario//
         datos.append("valorNombre", valorNombre);
         datos.append("valorProducto", valorProducto);
         datos.append("valorComentarios", valorComentarios); 
