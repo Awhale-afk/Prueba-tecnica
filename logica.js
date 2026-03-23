aae7b7aa97bfa64867ec532ff15632c22e5142e8
Emmanuel <est.emmgarcia.edu.co@gmail.com>
2026-03-22 21:52:36 -0500

    Documentación del js

--- logica.js
+++ logica.js
@@ -1,10 +1,9 @@
 let inputNombre = document.getElementById('nombre')
 let btnEnviar = document.getElementById('btnEnviar')
 let inputProducto = document.getElementById('productoId')
-let inputComentarios = document.getElementById('feedback')
+let inputComentarios = document.getElementById('feedback') //Se obtienen los inputs del front//
 
-btnEnviar.addEventListener('click', function (c) {
-    
+btnEnviar.addEventListener('click', function (c) { //event.listener relacionado con el botón de enviar para recoger los datos y enviarlos a Google Sheets//
     
     c.preventDefault();
 
@@ -14,22 +13,22 @@ btnEnviar.addEventListener('click', function (c) {
 
     //console.log(`${valorNombre} + ${valorProducto} + ${valorComentarios}`);// console.log para probar que el input recoja los datos
 
-    if (valorProducto == "" || valorNombre == "" || valorComentarios == ""){
-        alert('No se puede enviar un campo vacío')
+    if (valorProducto == "" || valorNombre == "" || valorComentarios == ""){ //En caso de que uno o más campos estén vacios el programa no permite enviar datos//
+        alert('No se puede enviar un campo vacío')                          //Con esto se evita el uso de tokens de manera innecesaria 
         return
     }else{
 
-        const URL_SCRIPT = "https://script.google.com/macros/s/AKfycbxBHOaRdCr7qfotrunHBG2psZoDEyxtEq-b_PSS4wCehFI4_msLb3t3PL2ss9s1DBGX/exec"
+        const URL_SCRIPT = "https://script.google.com/macros/s/AKfycbxBHOaRdCr7qfotrunHBG2psZoDEyxtEq-b_PSS4wCehFI4_msLb3t3PL2ss9s1DBGX/exec" //URL de Apps Script//
 
         let datos = new URLSearchParams();
         datos.append("valorNombre", valorNombre);
         datos.append("valorProducto", valorProducto);
-        datos.append("valorComentarios", valorComentarios);
+        datos.append("valorComentarios", valorComentarios); 
 
         fetch(URL_SCRIPT, {
             method: 'POST',
             body: datos.toString(),
-            mode: 'cors',
+            mode: 'cors',           //Los datos enviados se convierten a string en forma de URL para que sean legibles para la red//
             headers:{
                 'Content-Type': 'application/x-www-form-urlencoded'
             },
